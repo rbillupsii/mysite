285d4f4d0cd5531b10705fa6e67b30c5c6f9ac28
Roy Crowley <roybillups@comcast.net>
2019-07-30 20:19:18 -0700

    Added templates

---
+++ myblog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import stub_view
+from .views import list_view
+
+urlpatterns = [
+    path('', list_view, name="blog_index"),
+    path('posts/<int:post_id>/', stub_view, name="blog_detail"),
+]
