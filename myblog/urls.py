d09a97292c2c2c818d4c95ac4db41816e899522d
Roy Crowley <roybillups@comcast.net>
2019-07-30 20:46:12 -0700

    added login template

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import stub_view
 from .views import list_view
+from .views import detail_view
 
 urlpatterns = [
-    path('', list_view, name="blog_index"),
-    path('posts/<int:post_id>/', stub_view, name="blog_detail"),
+    path('', 
+        list_view,
+        name="blog_index"),
+    path('posts/<int:post_id>/',
+        detail_view,
+        name="blog_detail"),
 ]
